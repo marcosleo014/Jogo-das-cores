2fa5bb86ca6b51e47431afbb9f6bc5a33978ed5c
Marcos Leonan <marcosleo014@gmail.com>
2025-04-09 16:15:30 -0300

    melhorias visuais + nível aumentado

--- interacao.js
+++ interacao.js
@@ -1,63 +1,93 @@
 const quadrados = document.querySelectorAll(".quadrado");
 
-const cores = [1,1,1,1,1,1,1,1,1];
+const cores = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
 
-function incrementar(lista) {
-    lista.forEach(i => {
-        cores[i]++
-    })
+function selecionados(k, n) {
+    lista = [k];
+    const linha = k/n;
+    if (linha < 1) {
+        lista.push(k+n)
+    } else if (n-1 <= linha) {
+        lista.push(k - n)
+    } else {
+        lista.push(k - n, k + n)
+    };
+
+    const coluna = k % n;
+    if (coluna == 0) {
+        lista.push(k + 1)
+    } else if (coluna == 3) {
+        lista.push(k - 1)
+    } else {
+        lista.push(k - 1, k + 1)
+    };
+    return lista
 }
 
 function alterarCor(lista) {
     lista.forEach(q => {
+        cores[q]++
         if (cores[q] % 2 == 0) {
             quadrados[q].style.backgroundColor = "#fff"
+            console.log("essa porra mudou de cor, quadrado:", q) //não esquecer de tirar isso depois
         } else {
             quadrados[q].style.backgroundColor = "#000"
         }
     })
 }
+
+function destacar(lista) {
+    lista.forEach(q => {
+        quadrados[q].style.boxShadow = "0px 0px 5px 5px rgb(24, 189, 32)"
+        quadrados[q].style.transform = "scale(1.05)"
+    })
+}
+
+function resetDestacar(lista) {
+    lista.forEach(q => {
+        quadrados[q].style.boxShadow = "2px 2px 5px 2px #d0d0d0"
+        quadrados[q].style.transform = "scale(1)"
+        
+    })
+}
+
 quadrados.forEach(quadrado => {
+    
     quadrado.addEventListener("click", evento => {
         const quadradoClicado = Array.from(quadrados).indexOf(evento.target);
-        switch (quadradoClicado) {
-            case 0:
-                incrementar([0,1,3]);
-                alterarCor([0,1,3]);
-                break;
-            case 1:
-                incrementar([0,1,2,4]);
-                alterarCor([0,1,2,4]);
-                break;
-            case 2:
-                incrementar([1,2,5]);
-                alterarCor([1,2,5]);
-                break;
-            case 3:
-                incrementar([0,3,4,6]);
-                alterarCor([0,3,4,6]);
-                break;
-            case 4:
-                incrementar([1,3,4,5,7]);
-                alterarCor([1,3,4,5,7]);
-                break;
-            case 5:
-                incrementar([2,4,5,8]);
-                alterarCor([2,4,5,8]);
-                break;
-            case 6:
-                incrementar([3,6,7]);
-                alterarCor([3,6,7]);
-                break;
-            case 7:
-                incrementar([4,6,7,8]);
-                alterarCor([4,6,7,8]);
-                break;
-            case 8:
-                incrementar([5,7,8]);
-                alterarCor([5,7,8]);
-                break;
-        }
+        alterarCor(selecionados(quadradoClicado, 4))
+        verificar(4)
+    })
+
+    quadrado.addEventListener("mouseover", evento => {
+        const quadradoEvento = Array.from(quadrados).indexOf(evento.target);
+        console.log("entrou no quadrado:", quadradoEvento) //retirar depois
+        destacar(selecionados(quadradoEvento, 4))
+    })
+
+    quadrado.addEventListener("mouseleave", evento => {
+        const quadradoEvento = Array.from(quadrados).indexOf(evento.target);
+        console.log("saiu do quadrado:", quadradoEvento) //retirar depois
+        resetDestacar(selecionados(quadradoEvento, 4))
     })
 })
 
+
+const msg = document.querySelector(".msg")
+let brancos = 0;
+function verificar(n) {
+    brancos = 0;
+    for (const branco of cores) {
+        if (branco %2 == 0) {
+            brancos++
+        }
+    }
+    if (brancos == n) {
+        msg.innerHTML = "<h3>Parabéns! Você conseguiu.</h3>"
+        cores.forEach(q => {
+            q = 1
+        })
+    } else {
+        msg.innerHTML = "<strong>Objetivo:</strong> Altere as cores de todos os quadrados para branco."
+    }
+}
